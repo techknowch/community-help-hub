bb4e95cdcdc88703c5c6b570f21c7c4ec8e36eef
Syed Muhammad Ali Naqvi <techknowasia@proton.me>
2025-05-28 23:37:35 +0500

    Create app.js register auth routes

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", require("./modules/auth/auth.routes"));
+
+
+module.exports = app;
