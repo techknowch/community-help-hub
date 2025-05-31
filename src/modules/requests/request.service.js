e90b548d465151d04078bc6d7756b7f0064d5ed6
Syed Muhammad Ali Naqvi <techknowasia@proton.me>
2025-05-31 20:04:13 +0500

    Add new request to the queue in memory

--- src/modules/requests/request.service.js
+++ src/modules/requests/request.service.js
@@ -1,9 +1,13 @@
 const Request = require('./request.model');
+const requestQueue = require("./request.queue");
+
 
 exports.createRequest = async (requestData) => {
     try {
         const request = new Request(requestData);
         const savedRequest = await request.save();
+        requestQueue.enqueue(savedRequest); // Add to the queue
+        console.log(`Request created and added to queue: ${savedRequest._id}`);
         return savedRequest;
     } catch (error) {
         throw new Error(`Error creating request: ${error.message}`);
