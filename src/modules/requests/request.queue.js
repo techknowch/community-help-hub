f5d8c2ca628211103da7d80573c1d64db3dd5c22
Syed Muhammad Ali Naqvi <techknowasia@proton.me>
2025-05-31 20:39:27 +0500

    Import persistent queue class and initialize it in request queue

--- src/modules/requests/request.queue.js
+++ src/modules/requests/request.queue.js
@@ -1,5 +1,19 @@
 const Queue = require("../../utils/Queue");
+const PersistentQueue = require("../../utils/persistentQueue");
 
 const requestQueue = new Queue();
+const persistentQueue = new PersistentQueue("help_request_queue");
+
+(async () => {
+    try {
+        // Initialize the persistent queue
+        await persistentQueue.init("help_request_queue");
+        console.log("Request queue initialized successfully.");
+    } catch (error) {
+        console.error("Error initializing request queue:", error);
+    }
+})();
+
 
 module.exports = requestQueue;
+module.exports.persistentQueue = persistentQueue;
